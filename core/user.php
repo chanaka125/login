29774e9157ee7e1deb312940eb0547876a98d742
Chanaka Gunasekara <Chanaka Gunasekara@chanaka-laptop>
2015-09-20 11:27:23 +0530

    Start the test project

--- core/user.php
+++ core/user.php
@@ -1,8 +1,20 @@
 <?php
-namespace site1\core;
-class user{
-	public $name = "Hi I am user";
+
+class User{
+	private $text = null;
 	
+	
+	public function setText($body){
+		$this->text = $body." \n".date("Y/m/d");
+	}
+	
+	public function clearText() {
+		$this->text = null;
+	}
+	
+	public function getText() {
+		return $this->text;
+	}
 	
 }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-	
+include_once 'core/user.php';
+/* $date = date("Y/m/d");
+echo $date; */
+	$user1 = new User();
+	$user1->setText("this is body of the msg");
+	echo $user1->getText();
 	
